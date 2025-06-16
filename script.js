7dc2c26b1dc5fca88b032691eb3e43c83cd755cd
Feroan101 <feroanmothy666@gmail.com>
2025-06-16 20:32:23 +0530

    username catcher

--- script.js
+++ script.js
@@ -12,7 +12,11 @@ function submit(){
     let name = document.getElementById("username").value
     let pass = document.getElementById("pass").value
 
-    if (pass !== "7474"){
+    if (name == ""){
+        document.getElementById("waste").innerHTML = "<b>Enter Your Name</b>"
+    }
+
+    else if (pass !== "7474"){
         let waste = document.getElementById("waste")
         attempt += 1;
         waste.innerHTML = 'Wrong Password';
@@ -20,22 +24,17 @@ function submit(){
 
         if (attempt >= 5){
             waste.style.color = "black";
-            waste.style.textAlign = "center";
             waste.innerHTML = "how did you not figure this out "  + name + "? <br>There are clues all over the Webpage";
         }
 
         if (attempt >=10){
             waste.style.color = "black";
-            waste.style.textAlign = "center";
             waste.innerHTML = 'OMG STOP TRYING <br> Just ask <a style="color: blueviolet; cursor: pointer;font-family: "Quicksand", sans-serif; margin: 0;" href="https://www.instagram.com/feroan.xo/">me</a> ffs';
         } 
-
     }
-
     else{
         localStorage.setItem("attempt", attempt);
-        window.location.href = "actual.html";
-        
+        window.location.href = "actual.html";   
     }
 }
 
